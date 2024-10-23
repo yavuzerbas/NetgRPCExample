c4c6a61d295b978646d0a4fb2cc0f5995801d1f5
yavuzerbas <yavuz0erbas@gmail.com>
2024-10-23 13:01:15 +0300

    Create first example

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Terminal commands to create projects:
+
+dotnet new grpc --name grpcServer
+dotnet new console --name grpcClient
+
+client dependencies:
+Google.Protobuf
+Grpc.Net.Client
+Grpc.Tools
---
+++ grpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using grpcClient;
+/*using grpcClient;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5187");
+var greetClient = new Greeter.GreeterClient(channel);    
+
+HelloReply helloReply = await greetClient.SayHelloAsync(new HelloRequest{
+    Name = "Test greeting"
+});
+
+System.Console.WriteLine(helloReply.Message);
+*/
+
+var channel = GrpcChannel.ForAddress("http://localhost:5187");
+var greetClient = new Greeter.GreeterClient(channel);
+
+HelloReply result = await greetClient.SayHelloAsync(new HelloRequest{
+    Name = "Test user"
+});
+
+System.Console.WriteLine(result.Message);
